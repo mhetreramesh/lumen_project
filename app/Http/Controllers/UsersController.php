168bca7c54b76b242ed026acfbcb19ae866b4e07
Ramesh Mhetre <ramesh@web-flight.nl>
2016-03-03 12:09:58 +0530

    Sample API created

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\User;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class UsersController extends Controller
+{
+    public function index() {
+        $users = User::all();
+        return response()->json($users);
+    }
+}
